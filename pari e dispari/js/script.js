a12ecc947a8243da7cb0f3b639094d47ee18d914
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-16 15:50:46 +0100

    Complete: pari o dispari

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -13,9 +13,32 @@ do {
 
 console.log(`Hai scelto il numero ${userNumber}`);
 // Creo una funzione che mi genera un numero random da 1 a 5 per il computer
+function randomNmbr(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;    
+}
 
+// Creo la costante con il numero generato randomicamente
+let rndComNumb = randomNmbr(1, 5)
+console.log(`Il computer ha scelto il numero ${rndComNumb}`);
 // Sommiamo il numero dell'utente con il numero creato per il computer
-
+let sum;
+sum = parseInt(rndComNumb + userNumber);
+console.log(`La somma dei vostri numeri è: ${sum}`);
 // Stabiliamo se la somma dei numeri è pari o dispari attraverso una funzione
 
+function isOddOrEven(number) {
+    if (sum % 2 === 0) {
+        return "pari"
+    } else {
+        return "dispari"
+    }
+}
+
+let result = isOddOrEven(sum)
+console.log(result);
 // Dichiaro chi ha vinto
+if (result === userChoice) {
+    console.log(`Hai vinto!`);
+} else {
+    console.log(`Hai perso!`);
+}
