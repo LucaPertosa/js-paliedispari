d548f27a3e96449be1c2d6205cc3507356f97ac4
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-16 15:28:20 +0100

    Complete: pari e dispari prompt + controlli inserimento

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,7 +1,17 @@
 // Chiedo all'utente di scegliere tra pari o dispari
+let userChoice;
+do {
+    userChoice = prompt("Scegli tra pari o dispari").toLowerCase();
+} while (userChoice !== "pari" && userChoice !== "dispari");
 
+console.log(`Hai scelto l'opzione ${userChoice}`);
 // Chiedo all'utente di inserire un numero compreso tra 1 e 5
+let userNumber;
+do {
+    userNumber = parseInt(prompt("Scegli un numero da 1 a 5"))
+} while (userNumber < 1 || userNumber > 5);
 
+console.log(`Hai scelto il numero ${userNumber}`);
 // Creo una funzione che mi genera un numero random da 1 a 5 per il computer
 
 // Sommiamo il numero dell'utente con il numero creato per il computer
