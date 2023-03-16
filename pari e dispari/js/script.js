d1d74fbed076ef231cdf293b5d0bd183ea73eda8
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-16 15:07:22 +0100

    Complete: ragionamento logico pari o dispari

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,11 @@
+// Chiedo all'utente di scegliere tra pari o dispari
+
+// Chiedo all'utente di inserire un numero compreso tra 1 e 5
+
+// Creo una funzione che mi genera un numero random da 1 a 5 per il computer
+
+// Sommiamo il numero dell'utente con il numero creato per il computer
+
+// Stabiliamo se la somma dei numeri è pari o dispari attraverso una funzione
+
+// Dichiaro chi ha vinto
