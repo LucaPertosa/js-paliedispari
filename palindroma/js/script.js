713c3bd50af90b55a2f138fb17fcd0d67570de31
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-16 13:42:54 +0100

    Create: project scaffolding & ragionamento logico di palindroma

---
+++ palindroma/js/script.js
@@ -0,0 +1,4 @@
+// Chiedere all'utente di inserire una parola
+const userWord = prompt("Ciao utente! Inserisci una parola e ti dirò se è palindroma");
+console.log(userWord);
+// Creare una funzione per capire se la parola inserita è palindroma.
