4c3d6ca5e77099c819f37e602699f1759212c5f0
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-16 14:58:09 +0100

    Complete: palindroma exercise

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,3 +2,20 @@
 const userWord = prompt("Ciao utente! Inserisci una parola e ti dirò se è palindroma");
 console.log(userWord);
 // Creare una funzione per capire se la parola inserita è palindroma.
+
+
+// Creo la fuznione
+function isPalindroma(userChoice) {
+    // Trasformo la parola dell'utente in un array dove ogni lettera indica un elemento e ne inverto l'ordine
+    let reverseWord = userChoice.split('').reverse().join('');
+    // Restituisco il valore booleano come risposta alla funzione
+    return reverseWord === userChoice;
+};
+
+// Stampo i messaggi 
+if (isPalindroma(userWord)) {
+    console.log('La parola inserita è palindroma');
+} else {
+    console.log('La parola inserita non è palindroma');
+};
+
